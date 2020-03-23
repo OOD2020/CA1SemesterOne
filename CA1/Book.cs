e4a327a2f5d544d3721f2c99e845bbef995762d2
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-23 21:09:23 +0000

    Add project files.

---
+++ CA1/Book.cs
@@ -0,0 +1,53 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date : 5/ 11/ 2019
+ * Description : CA solution
+ * 
+ * ############################# */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    //enum
+    public enum Genre { Fiction, Biography, Computing, Literature }
+    class Book
+    {
+        //properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Type { get; set; }
+        //end of properties
+
+
+
+
+         //Constructor
+        public Book(string title, string author, int publishDate, string type)
+
+        {
+            Title = title;
+            Author = author;
+            PublishDate = new DateTime(publishDate);
+            Enum.TryParse(type, out Genre genre);
+
+
+        }
+      
+        //ToString method
+        public override string ToString()
+        {
+            return string.Format("Title: {0} Author: {1} PublishDate: {2}",
+                Title, Author, PublishDate);
+        }//end of method
+        public string BookList()
+        {
+            return string.Format($"{Title,-15}{Author,-7}{PublishDate,-7}");
+        }
+    }
+}
---
+++ CA1/Program.cs
@@ -0,0 +1,51 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date : 5/ 11/ 2019
+ * Description : CA solution
+ * 
+ * ############################# */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //list initialiser
+            List<Book> ReadingList = new List<Book>();
+
+            //creating 5 objects
+            Book book1 = new Book("James Joyce", "Ulysses ", 2 / 21 / 922, "Literature");
+            Book book2 = new Book(" B Kernighan and D Ritchie ", "The C Programming Language ", 28 / 02 / 1978, "Computing");
+            Book book3 = new Book("Walter Isaacson ", " Steve Jobs ", 24 / 10 / 2011, "Biography");
+            Book book4 = new Book(" JRR Tolkien", "The Hobbit  ", 21 / 09 / 1937, "Fiction");
+            Book book5 = new Book("James Joyce", "Portrait of the Artist ", 29 / 12 / 1916, "Literature");
+
+            //adding the objects to the list
+            ReadingList.Add(book1);
+            ReadingList.Add(book2);
+            ReadingList.Add(book3);
+            ReadingList.Add(book4);
+            ReadingList.Add(book5);
+            Display(ReadingList);
+
+        }//end of main method 
+
+        //Displaying the entire list
+        static void Display(List<Book> books)
+        {
+            Console.WriteLine("{0,-15}{1,-7}{2,-7}{3,-7}{4,-7}{5,-7}", "NAME", "AUTHOR", "PUBLISH DATE", "GENRE");
+
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book.BookList());
+            }
+        }//end of method
+    }
+}
